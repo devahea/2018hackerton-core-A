30fb48cb63ed028c3bdacb87d283fe410dfc8971
김종인(Jongin Kim)/Sell개발팀/SKP <kji6252@sk.com>
2018-09-09 01:59:08 +0900

    Block VO 전체 생선자 추가

--- src/main/java/ahea/hackerton/nurikabe/dto/Block.java
+++ src/main/java/ahea/hackerton/nurikabe/dto/Block.java
@@ -1,8 +1,10 @@
 package ahea.hackerton.nurikabe.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Block {
     private Position position;
     private int number;
