4efb7389cdc2f5050bc999998808417297f8b58b
thdghgns <thdghgns@gmail.com>
2018-09-09 01:51:33 +0900

    add SolveService impl

---
+++ src/main/java/ahea/hackerton/nurikabe/service/SolveServiceImpl.java
@@ -0,0 +1,25 @@
+package ahea.hackerton.nurikabe.service;
+
+import ahea.hackerton.nurikabe.dto.Block;
+import ahea.hackerton.nurikabe.dto.Position;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SolveServiceImpl implements SolveService {
+    @Override
+    public List<Block> level1() {
+        return null;
+    }
+
+    @Override
+    public List<List<Position>> level2(List<Block> blocks) {
+        return null;
+    }
+
+    @Override
+    public List<List<Position>> level3(List<Block> blocks, List<List<Position>> positionsList) {
+        return null;
+    }
+}
