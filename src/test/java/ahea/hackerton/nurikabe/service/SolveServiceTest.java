f76fc15e5da434fb1ea0c390129e16f344c0fef8
thdghgns <thdghgns@gmail.com>
2018-09-09 01:48:16 +0900

    add test code

--- src/test/java/ahea/hackerton/nurikabe/service/SolveServiceTest.java
+++ src/test/java/ahea/hackerton/nurikabe/service/SolveServiceTest.java
@@ -4,11 +4,16 @@ import ahea.hackerton.nurikabe.dto.Block;
 import ahea.hackerton.nurikabe.dto.Position;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class SolveServiceTest {
 
     private int[][] problem;
@@ -22,9 +27,12 @@ public class SolveServiceTest {
                         {3,0,0,0,5},
                         {0,1,0,0,0},
                         {0,0,0,0,0}};
+
+        this.problem = problem;
     }
 
     @Test
     public void level1() {
+        System.out.println(problem);
     }
 }
