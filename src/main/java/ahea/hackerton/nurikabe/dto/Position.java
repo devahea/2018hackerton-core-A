65cf7c9a6c49769b50bd734f846fad97d08c2c62
김종인(Jongin Kim)/Sell개발팀/SKP <kji6252@sk.com>
2018-09-09 02:00:00 +0900

    Position VO 전체 생선자 추가

--- src/main/java/ahea/hackerton/nurikabe/dto/Position.java
+++ src/main/java/ahea/hackerton/nurikabe/dto/Position.java
@@ -1,8 +1,10 @@
 package ahea.hackerton.nurikabe.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Position {
     private int x;
     private int y;
